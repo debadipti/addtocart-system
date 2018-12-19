87d242e35d6420c3a0b1cc7fc93ec3de813e0822
Debadipti Patra <pdebadipti@yahoo.com>
2018-12-19 12:39:03 +0530

    commit #2 style and cart button click event update

--- main.js
+++ main.js
@@ -1,11 +1,56 @@
-const pQuant = document.querySelectorAll(".p-quant");
+// get elements from the dom
+const pQuant = document.getElementsByClassName("p_quant");
+const pAdd = document.getElementsByClassName("p_add");
+const pCount = document.getElementsByClassName("p_count");
+const pRemove = document.getElementsByClassName("p_remove");
+const cartAction = document.getElementsByClassName("cart-action");
 
-for (i = 0; i < pQuant.length; i++) {
-  pQuant[i].addEventListner("click", incQuant);
+// increase the pCount value
+for (i = 0; i < pAdd.length; i++) {
+  pAdd[i].addEventListener("click", increaseCount);
+  function increaseCount(e) {
+    const parent = e.target.parentElement.parentElement;
+    const child = parent.children;
+    const childArray = Array.from(child);
+    let incCount = childArray[1].innerHTML;
+    if (incCount >= 0) {
+      incCount++;
+    }
+    childArray[1].innerHTML = incCount;
+  }
 }
 
-function incQuant(e) {
-  if (e.target.className.contains("q_add")) {
-    console.log("hi");
+// decrease the pCount value
+for (i = 0; i < pRemove.length; i++) {
+  pRemove[i].addEventListener("click", decreaseCount);
+  function decreaseCount(e) {
+    const parent = e.target.parentElement.parentElement;
+    const child = parent.children;
+    const childArray = Array.from(child);
+    let incCount = childArray[1].innerHTML;
+    if (incCount > 0) {
+      incCount--;
+    }
+    childArray[1].innerHTML = incCount;
+  }
+}
+
+// add/delete action button
+for (i = 0; i < cartAction.length; i++) {
+  cartAction[i].addEventListener("click", deleteAction);
+  function deleteAction(e) {
+    let targetElement = e.target.innerHTML;
+    if (targetElement === "delete") {
+      e.target.innerHTML = "add_circle";
+      e.target.parentElement.parentElement.children[1].children[1].innerHTML = 0;
+      e.target.parentElement.parentElement.children[1].style.visibility =
+        "hidden";
+    }
+    if (targetElement === "add_circle") {
+      e.target.innerHTML = "delete";
+      e.target.parentElement.parentElement.children[1].children[1].innerHTML = 1;
+      e.target.parentElement.parentElement.children[1].style.visibility =
+        "visible";
+    }
   }
 }
